70ea5365c97a9ceee1e053f1038118bbcf97797c
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-01-15 09:38:25 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () => {
+  return (
+    <div> cuhp 2005</div>
+  )
+}
 const App = () => {
   const [count, setCount] = useState(0)
 
@@ -17,6 +22,7 @@ const App = () => {
         </a>
       </div>
       <h1>I'm Phuc</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
