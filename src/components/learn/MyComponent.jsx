3db5048e3cc32bf7e7d1d819074f55d233b72475
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-01-15 14:44:00 +0700

    video 31- Cach viet js trong html

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,13 @@
 import './style.css'
 const MyComponent = () => {
+    // const phuc = 'Nguyen Dang Phuc';
+    const phuc = {
+        name: 'Nguyen Dang Phuc',
+        age: 19
+    }
     return (
         <>
-            <div> cuhp 2005</div>
+            <div> {JSON.stringify(phuc)} 2005</div>
             <div className="child">20/11/2005</div>
         </>
     )
