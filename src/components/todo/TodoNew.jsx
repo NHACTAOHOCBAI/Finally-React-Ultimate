eb41d61ffd0edf7a82cac49bb4444eaa252c16bb
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-01-15 19:36:21 +0700

    video 36 - DOM events

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,20 @@
 const TodoNew = (props) => {
     const { addNewTodo } = props;
-    addNewTodo('Phuc');
+    const handleClick = () => {
+        alert("Click me");
+    }
+    const handleOnChange = (name) => {
+        console.log(name);
+    }
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => { handleOnChange(event.target.value) }}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
