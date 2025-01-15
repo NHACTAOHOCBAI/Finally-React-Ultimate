e691fd7679dfd0fda474a45b2f6abcd6b4895ac0
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-01-15 10:30:23 +0700

    video 30-JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,10 @@
+import './style.css'
 const MyComponent = () => {
     return (
-        <div> cuhp 2005</div>
+        <>
+            <div> cuhp 2005</div>
+            <div className="child">20/11/2005</div>
+        </>
     )
 }
 export default MyComponent
