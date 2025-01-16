5bae5164ff07716d26c05593f5c75696b654ad67
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-01-16 17:07:02 +0700

    video 40 - Them key

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -4,7 +4,7 @@ const TodoData = (props) => {
         <div className="todo-data">
             {todoList.map((value, index) => {
                 return (
-                    <div className="todo-item">
+                    <div className="todo-item" key={value.id}>
                         <div>{value.name}</div>
                         <button>delete</button>
                     </div>
