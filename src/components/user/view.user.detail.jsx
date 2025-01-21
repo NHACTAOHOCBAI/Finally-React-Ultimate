c004714defd6a6139473bd9c24dd441b1a61defd
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-01-21 21:02:00 +0700

    video 83 - xu ly previous view

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,7 +1,22 @@
 import { Drawer } from "antd"
+import { useState } from "react";
 
 const ViewDetail = (props) => {
     const { setDataView, dataView, isModalDetailOpen, setIsModalDetailOpen } = props
+    const [selectedFile, setSelectedFile] = useState();
+    const [preview, setPreview] = useState()
+
+    const handleOnChangeFile = async (event) => {
+        if (!event.target.files || event.target.files.length == 0) {
+            // setSelectedFile(null);
+            // setPreview(null);
+            return;
+        }
+
+        const file = event.target.files[0];
+        setSelectedFile(file);
+        setPreview(URL.createObjectURL(file));
+    }
     return (
         <Drawer title="Basic Drawer"
             open={isModalDetailOpen}
@@ -15,11 +30,15 @@ const ViewDetail = (props) => {
                     <p>{`Title :${dataView.title}`}</p>
                     <p>{`Body :${dataView.body}`}</p>
                     <div>
-                        <img src={`${import.meta.env.VITE_BACKEND_URL}/images/${dataView.avatar}`} />
+                        <img src={preview} />
                     </div>
                     <div>
                         <label htmlFor="btnUpload">Up Load file</label>
-                        <input type="file" hidden id='btnUpload' />
+                        <input type="file"
+                            hidden id='btnUpload'
+                            onChange={handleOnChangeFile}
+                        // nhung tham so dau vao cua onchange se tu dong vao ham kia
+                        />
                     </div>
                 </>
                 :
