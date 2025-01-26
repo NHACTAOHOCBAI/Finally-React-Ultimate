86bba40f40deace6234b95fee600d7ccea27f35d
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-01-26 18:31:24 +0700

    video 89 su dung uncontrolled components cho register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,77 @@
+import { BugTwoTone } from "@ant-design/icons";
+import { Button, Form, Input } from "antd"
+
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => {
+        console.log(values)
+    }
     return (
-        <div>Register Page</div>
+        <Form
+            form={form}
+            layout='vertical'
+            onFinish={onFinish}
+        >
+            <div style={{
+                margin: "50px"
+            }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone Number"
+                    name="phone"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <div>
+                    <Button
+                        type='primary'
+                        onClick={() => form.submit()}
+                    >Register</Button>
+                </div>
+            </div>
+        </Form>
     )
 }
 export default RegisterPage
